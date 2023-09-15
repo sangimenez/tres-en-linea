6e578703f97be7a553d29830eb4cbdc56fbfb3f6
Sandra <sandragimenez@outlook.es>
2023-09-15 17:41:59 +0200

    history of game

--- src/App.js
+++ src/App.js
@@ -13,10 +13,7 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board({xIsNext, squares, onPlay}) {
   function handleClick(i) {
     //verificamos el estado del cuadrado, si esta ocupado no entra y si es ganador
     if(squares[i] || calculateWinner(squares)){
@@ -29,15 +26,13 @@ export default function Board() {
     }else{
       nextSquares[i]='O';
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
-
   const winner = calculateWinner(squares);
   let status;
-  if(winner){
-    status = 'Ganador:' + winner;
-  }else{
+  if (winner) {
+    status = 'Ganador: ' + winner;
+  } else {
     status = 'Siguiente jugador: ' + (xIsNext ? 'X' : 'O');
   }
   return (
@@ -63,6 +58,48 @@ export default function Board() {
   );
 }
 
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Ir hacia la jugada #' + move;
+    } else {
+      description = 'Ir al inicio del juego';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
 function calculateWinner(squares){
   //declaramos las posibles combinaciones del gaandor tanto si es O o X
   const lines = [
@@ -75,9 +112,9 @@ function calculateWinner(squares){
     [0,4,8], 
     [2,4,6]
   ];
-  for (let i=0; i<lines.length; i++){
-    const [a,b,c] = lines[i];
-    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
